a2751c8a995cc46ac56ba57fe4471e4a16d77438
Huki4n <ryurickryurik@yandex.ru>
2023-09-22 17:58:22 +0300

    Миу миу

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-def calculator():
-    while(True):
-        print("Если хотите выйти в 2 числах напише 0\n")
-        # Запрос чисел
-        num1 = int(input("Введите первое число: "))
-        num2 = int(input("Введите второе число: "))
-        # Выход из калькулятора
-        if num1 == num2 == 0:
-            print("\nСпасибо за использование моего калькулятора)))")
-            break
-        # Запрос действия
-        action = str(input("Введите действие(*  /  -  +): "))
-        # Функционал калькулятора
-        if action == "+":
-            print("\nОтвет - ",num1+num2,"\n\n")
-        elif action == "-":
-            print("\nОтвет - ",num1-num2,"\n\n")
-        elif action == "/":
-            if num2 != 0:
-                print("\nОтвет - ",num1 / num2,"\n\n")
-            else:
-                print("\nНа ноль делить нельзя","\n\n")
-        elif action == "*":
-            print("\nОтвет - ",num1*num2,"\n\n")
-
-# Функция вызова
-calculator()
+for i in range(1,100):
+    print("МИУ МИУ")    
 
 
 
